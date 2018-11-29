77df9bc6f9b59eefe13a30b46cbf303f32e45357
Vanessa Prema <vanessabprema@gmail.com>
2018-11-29 14:28:07 -0500

    Math Lab

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,34 @@
 def addition(num1, num2)
+    add = num1 + num2
+    add
 end
 
 def subtraction(num1, num2)
+    sub = num1 - num2
+    sub
 end
 
 def division(num1, num2)
+    div = num1/num2
+    div
 end
 
 def multiplication(num1, num2)
+    mul = num1*num2
+    mul
 end
 
 def modulo(num1, num2)
+    mod = num1%num2
+    mod
 end
 
 def square_root(num)
+    sq = Math.sqrt(num)
+    sq
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+    ans = num1 + ((num2*num3)/num4)
+    ans
 end
